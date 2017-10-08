ca604f240e3e6522072a3c6b2305a4ba24dc45b8
Guillaume Monet <guillaume.monet@free.fr>
2017-10-08 23:14:12 +0200

    Comments on sftpconnection class

--- src/Rad/Utils/SFTPConnection.php
+++ src/Rad/Utils/SFTPConnection.php
@@ -4,32 +4,48 @@ namespace Rad\Utils;
 
 use Exception;
 
+/**
+ * @author Guillaume Monet
+ */
 class SFTPConnection {
 
     private $connection;
     private $sftp;
     private $host;
 
-    public function __construct($host, $port = 22) {
+    public function __construct(string $host, int $port = 22) {
         $this->host = $host;
         $this->connection = @ssh2_connect($host, $port);
         if (!$this->connection) {
-            throw new Exception("Could not connect to $host on port $port.");
+            throw new ErrorException("Could not connect to $host on port $port.");
         }
     }
 
-    public function login($username, $password) {
+    /**
+     * 
+     * @param string $username
+     * @param string $password
+     * @throws ErrroException
+     * @throws ErrorException
+     */
+    public function login(string $username, string $password) {
         if (!@ssh2_auth_password($this->connection, $username, $password)) {
-            throw new Exception("Could not authenticate with username $username and password $password.");
+            throw new ErrroException("Could not authenticate with username $username and password $password.");
         }
 
         $this->sftp = @ssh2_sftp($this->connection);
         if (!$this->sftp) {
-            throw new Exception("Could not initialize SFTP subsystem.");
+            throw new ErrorException("Could not initialize SFTP subsystem.");
         }
     }
 
-    public function uploadFile($local_file, $remote_file) {
+    /**
+     * 
+     * @param string $local_file
+     * @param string $remote_file
+     * @throws Exception
+     */
+    public function uploadFile(string $local_file, string $remote_file) {
         $stream = fopen("ssh2.sftp://" . intval($this->sftp) . "/./in/$remote_file", 'w');
 
         if (!$stream) {
@@ -48,14 +64,21 @@ class SFTPConnection {
         fclose($stream);
     }
 
-    public function downloadFile($local_file, $remote_file) {
+    /**
+     * 
+     * @param string $local_file
+     * @param string $remote_file
+     * @throws Exception
+     * @throws ErrorException
+     */
+    public function downloadFile(string $local_file, string $remote_file) {
         $stream = fopen("ssh2.sftp://" . intval($this->sftp) . "/./$remote_file", 'r');
         if (!$stream) {
             throw new Exception("Could not open file: $remote_file");
         }
         $local = fopen($local_file, 'w');
         if (!$local) {
-            throw new Exception("Could not open local file: $local_file.");
+            throw new ErrorException("Could not open local file: $local_file.");
         }
 
         $read = 0;
@@ -63,7 +86,7 @@ class SFTPConnection {
         while (($read < $filesize) && ($buffer = fread($stream, $filesize - $read))) {
             $read += strlen($buffer);
             if (fwrite($local, $buffer) === FALSE) {
-                throw new Exception("Could not write data to file: $local_file.");
+                throw new ErrorException("Could not write data to file: $local_file.");
             }
         }
         fflush($local);
@@ -71,11 +94,17 @@ class SFTPConnection {
         fclose($stream);
     }
 
-    public function listDirectory($directory) {
+    /**
+     * 
+     * @param string $directory
+     * @return array
+     * @throws ErrorException
+     */
+    public function listDirectory(string $directory) {
         $files = array();
         $stream = opendir("ssh2.sftp://" . intval($this->sftp) . "/./$directory");
         if (!$stream) {
-            throw new Exception("Could not open directory: $directory");
+            throw new ErrorException("Could not open directory: $directory");
         }
         while (false != ($entry = readdir($stream))) {
             if (!in_array($entry, array(".", ".."))) {
