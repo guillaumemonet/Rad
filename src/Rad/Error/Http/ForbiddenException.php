19b17e56d8de97ae30145547b8239a3830f4810e
Guillaume Monet <guillaume.monet@free.fr>
2017-10-23 22:34:24 +0200

    Correct Exception

---
+++ src/Rad/Error/Http/ForbiddenException.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Guillaume Monet.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+namespace Rad\Error\Http;
+/**
+ * Represents an HTTP 403 error.
+ */
+class ForbiddenException extends HttpException {
+
+    /**
+     * Constructor
+     *
+     * @param string $message If no message is given 'Forbidden' will be the message
+     * @param string $code Status code, defaults to 403
+     */
+    public function __construct($message = null, $code = 403) {
+        if (empty($message)) {
+            $message = 'Forbidden';
+        }
+        parent::__construct($message, $code);
+    }
+
+}
