5cb203d88cc5fe630d8fc9f23f9d6ed1c45409b5
Guillaume Monet <guillaume.monet@free.fr>
2017-11-28 23:35:09 +0100

    Change Test File

--- test/TestBase.php
+++ test/TestBase.php
@@ -2,9 +2,14 @@
 
 require(__DIR__ . "/../vendor/autoload.php");
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Rad\Api;
 use Rad\Config\Config;
 use Rad\Controller\Controller;
+use Rad\Log\Log;
+use Rad\Route\Route;
+use Rad\Utils\Time;
 
 /*
  * The MIT License
@@ -31,38 +36,68 @@ use Rad\Controller\Controller;
  */
 
 /**
- * Description of TestBase
+ * Simple example for testing purpose
  *
  * @author guillaume
  */
-class App extends Api {
-
-    public function addControllers(): array {
-        return array(
-            MyController::class
-        );
-    }
-
-}
-
 class MyController extends Controller {
 
     /**
-     * @api 0
-     * @get /^$|^\/$/
+     * @api 1
+     * @get /helloworld/html/show/
      * @produce html
      */
     public function helloWorld() {
         return "<b>Hello World</b>";
     }
 
+    /**
+     * @api 1
+     * @get /helloworld/json/show/
+     * @middleware Rad\Middleware\Base\Pre_CheckConsume
+     * @middleware Rad\Middleware\Base\Post_SetProduce
+     * @produce json
+     */
+    public function jsonHelloWorld() {
+        //$this->response = $this->response->withAddedHeader('Hello', 'Moto');
+        
+        return array("Hello World");
+    }
+
+    /**
+     * @api 1
+     * @get /helloworld/([aA-zZ]*)/display/(.*)/
+     * @middleware Rad\Middleware\Base\Pre_CheckConsume
+     * @middleware Rad\Middleware\Base\Post_SetProduce
+     * @produce html
+     */
+    public function namedHelloWorld() {
+        return '<b>Hello World</b> ' . $route->getArgs()[0] . " to " . $this->getRoute()->getArgs()[1];
+    }
+
+    /**
+     * @api 1
+     * @get /server/
+     * @middleware Rad\Middleware\Base\Pre_CheckConsume
+     * @middleware Rad\Middleware\Base\Post_SetProduce
+     * @produce html
+     * @consume json
+     */
+    public function serverRequest(ServerRequestInterface $request, ResponseInterface $response, Route $route) {
+        return print_r($request->getHeader('HTTP_ACCEPT')[0], true);
+    }
+
 }
 
+$time = Time::get_microtime();
 Config::set("cache", "type", "file");
 Config::set("cache", "enabled", 0);
 Config::set("log", "type", "file");
 Config::set("log", "error", 1);
 Config::set("log", "debug", 1);
 Config::set("log", "enabled", 1);
-$app = new App();
-$app->run();
+$app = new Api();
+$app->addControllers([MyController::class])
+        ->run();
+$ltime = Time::get_microtime();
+Log::getHandler()->debug("API REQUEST [" . round($ltime - $time, 10) * 1000 . "] ms");
