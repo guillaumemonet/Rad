31f3e33a4010161c8deee93ee63f6157c29ea57a
Guillaume Monet <guillaume.monet@free.fr>
2018-01-17 15:21:46 +0100

    Add Composer Manager class for bin script

--- src/Rad/Composer/Manager.php
+++ src/Rad/Composer/Manager.php
@@ -37,24 +37,54 @@ use RuntimeException;
 abstract class Manager {
 
     public static function createProject($name = null) {
-        if (!isset($name)) {
-            throw new RuntimeException("Missing Rad Project name");
+        if (!isset($name) || $name === null) {
+            throw new RuntimeException("Missing RAD project name");
         }
-        $projectName = StringUtils::camelCase($name);
+        $serviceName = StringUtils::camelCase($name);
         mkdir('cache');
         mkdir('datas');
         mkdir('config');
         mkdir('public');
         mkdir('src');
-        mkdir('src/' . $projectName);
-        mkdir('src/' . $projectName . '/Controllers');
-        mkdir('src/' . $projectName . '/Controllers/Base');
-        mkdir('src/' . $projectName . '/Datas');
-        mkdir('src/' . $projectName . '/Middlewares');
-        mkdir('src/' . $projectName . '/Codecs');
-        mkdir('assets/templates', umask(), true);
+        mkdir('src/' . $serviceName);
+        mkdir('src/' . $serviceName . '/Controllers');
+        mkdir('src/' . $serviceName . '/Controllers/Base');
+        mkdir('src/' . $serviceName . '/Datas');
+        mkdir('src/' . $serviceName . '/Datas/DAO');
+        mkdir('src/' . $serviceName . '/Datas/Imp');
+        mkdir('src/' . $serviceName . '/Middlewares');
+        mkdir('src/' . $serviceName . '/Codecs');
+        mkdir('assets/');
+        mkdir('assets/templates');
+        mkdir('assets/pictures');
         mkdir('jobs');
-        mkdir('logs');
+        mkdir('log');
+        copy(__DIR__ . '/../../../config/config.dist.json', 'config/config.json');
+    }
+
+    public static function buildProject($name) {
+        
+    }
+
+    public static function createService(string $serviceType = null, string $serviceName = null) {
+        if ($serviceType === null || $serviceName === null) {
+            throw new RuntimeException("Missing RAD service name");
+        }
+        $serviceType = StringUtils::camelCase($serviceType);
+        $serviceName = StringUtils::camelCase($serviceName);
+        mkdir('src');
+        mkdir('src/Rad');
+        mkdir('src/Rad/' . $serviceType);
+        if (!file_exists('src/Rad/' . $serviceType . '/' . $serviceType . '.php')) {
+            touch('src/Rad/' . $serviceType . '/' . $serviceType . '.php');
+        }
+        if (!file_exists('src/Rad/' . $serviceType . '/' . $serviceType . 'Interface.php')) {
+            touch('src/Rad/' . $serviceType . '/' . $serviceType . 'Interface.php');
+        }
+        if (!file_exists('src/Rad/' . $serviceType . '/' . $serviceName . '' . $serviceType . 'Handler.php')) {
+            touch('src/Rad/' . $serviceType . '/' . $serviceName . '' . $serviceType . 'Handler.php');
+        }
+        copy(__DIR__ . '/../../../config/config.dist.json', 'config/config.json');
     }
 
 }
