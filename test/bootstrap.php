c8e604b788a26e7cd7d1af197bc0c1eff53f4591
Guillaume Monet <guillaume.monet@free.fr>
2018-01-19 12:37:05 +0100

    Correct Test

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-Rad\Config\Config::load(__DIR__ . '/../config/config.json');
+Rad\Config\Config::load(__DIR__ . '/../config/config.default.json');
