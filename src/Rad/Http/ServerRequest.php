7a0e2990e636c865e446490915734c80eed8ad73
Guillaume Monet <guillaume.monet@free.fr>
2018-03-06 22:56:17 +0100

    Override fromglobals

--- src/Rad/Http/ServerRequest.php
+++ src/Rad/Http/ServerRequest.php
@@ -26,6 +26,7 @@
 
 namespace Rad\Http;
 
+use GuzzleHttp\Psr7\LazyOpenStream;
 use GuzzleHttp\Psr7\ServerRequest as GServerRequest;
 
 /**
@@ -38,7 +39,19 @@ class ServerRequest extends GServerRequest {
     use RequestTrait;
 
     public static function fromGlobals() {
-        return parent::fromGlobals();
+        $method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'GET';
+        $headers = function_exists('getallheaders') ? getallheaders() : [];
+        $uri = self::getUriFromGlobals();
+        $body = new LazyOpenStream('php://input', 'r+');
+        $protocol = isset($_SERVER['SERVER_PROTOCOL']) ? str_replace('HTTP/', '', $_SERVER['SERVER_PROTOCOL']) : '1.1';
+
+        $serverRequest = new ServerRequest($method, $uri, $headers, $body, $protocol, $_SERVER);
+
+        return $serverRequest
+                        ->withCookieParams($_COOKIE)
+                        ->withQueryParams($_GET)
+                        ->withParsedBody($_POST)
+                        ->withUploadedFiles(self::normalizeFiles($_FILES));
     }
 
 }
