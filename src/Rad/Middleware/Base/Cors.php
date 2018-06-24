5c30b2927a236335e706bb612059bbf64cb97b73
Guillaume Monet <guillaume.monet@free.fr>
2018-06-24 23:45:31 +0200

    Add Cors middleware

---
+++ src/Rad/Middleware/Base/Cors.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2018 guillaume.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace Rad\Middleware\Base;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Rad\Middleware\MiddlewareBefore;
+use Rad\Route\Route;
+
+/**
+ * Description of Cors
+ *
+ * @author guillaume
+ */
+class Cors extends MiddlewareBefore {
+
+    //put your code here
+    public function middle(ServerRequestInterface $request, ResponseInterface $response, Route $route): ResponseInterface {
+        if ($this->request->getHeader('REQUEST_METHOD') == 'OPTIONS' && (
+                $this->request->getHeader('HTTP_ACCESS_CONTROL_REQUEST_METHOD') &&
+                in_array($this->request->getHeader('HTTP_ACCESS_CONTROL_REQUEST_METHOD'), ['POST', 'DELETE', 'PUT', 'GET'])
+                )
+        ) {
+            $response = new Response(200, 'OK');
+            $response->send();
+            exit;
+        }
+    }
+
+}
