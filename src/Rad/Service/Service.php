3a11a91292c6a74206a6fafad1e234ddc9fede37
Guillaume Monet <guillaume.monet@free.fr>
2021-05-02 15:50:03 +0200

    Add debug , reformat code

--- src/Rad/Service/Service.php
+++ src/Rad/Service/Service.php
@@ -37,12 +37,12 @@ use Rad\Error\ConfigurationException;
  */
 abstract class Service implements ServiceInterface {
 
-    protected static $instances = [];
-    protected $serviceType = null;
+    protected static $instances  = [];
+    protected $serviceType       = null;
     protected $providedClassName = null;
-    protected $default = null;
-    protected $services = [];
-    protected $handlers = [];
+    protected $default           = null;
+    protected $services          = [];
+    protected $handlers          = [];
 
     protected function __construct() {
         $this->serviceType = $this->getServiceType();
@@ -84,7 +84,7 @@ abstract class Service implements ServiceInterface {
             if (!static::hasService($handlerType)) {
                 throw new ErrorException('Service ' . $handlerType . ' Not Found');
             }
-            $instance = new $this->services[$handlerType];
+            $instance                     = new $this->services[$handlerType];
             $this->handlers[$handlerType] = $instance instanceof $this->providedClassName ? $instance : null;
         }
         return $this->handlers[$handlerType];
@@ -100,16 +100,18 @@ abstract class Service implements ServiceInterface {
 
     private function loadConfig() {
         $config = Config::getServiceConfig($this->serviceType);
+
         $this->default = $config->default;
         if ($this->default === null) {
-            throw new ConfigurationException('No default handler defined');
+            throw new ConfigurationException('No default handler defined ' . $this->serviceType);
         }
         $this->providedClassName = $config->classname;
         if ($this->providedClassName === null) {
-            throw new ConfigurationException('No Provided Class defined');
+            throw new ConfigurationException('No Provided Class defined' . $this->serviceType);
         }
         $handlers = $config->handlers;
-        $this->services = array_combine(array_keys((array) $handlers), array_map(function($handler) {
+
+        $this->services = array_combine(array_keys((array) $handlers), array_map(function ($handler) {
                     return $handler->classname;
                 }, (array) $handlers));
     }
