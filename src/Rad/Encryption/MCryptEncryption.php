0f4df3f4878b0debbacb06460dc8ca03968ef42f
Guillaume Monet <guillaume.monet@free.fr>
2023-02-11 10:01:30 +0100

    Change Licence Format

--- src/Rad/Encryption/MCryptEncryption.php
+++ src/Rad/Encryption/MCryptEncryption.php
@@ -1,27 +1,10 @@
 <?php
 
-/*
- * The MIT License
- *
- * Copyright 2018 guillaume.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+/**
+ * @license http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
+ * @author Guillaume Monet
+ * @link https://github.com/guillaumemonet/Rad
+ * @package Rad
  */
 
 namespace Rad\Encryption;
@@ -36,7 +19,7 @@ use Rad\Config\Config;
 class MCryptEncryption implements EncryptionInterface {
 
     private static $cipher = MCRYPT_RIJNDAEL_128;
-    private static $mode = 'cbc';
+    private static $mode   = 'cbc';
 
     /**
      * Encrypt Datas
@@ -45,8 +28,8 @@ class MCryptEncryption implements EncryptionInterface {
      */
     public function crypt(string $data) {
         $keyHash = md5(Config::getConfig()->encrypt->key);
-        $key = substr($keyHash, 0, mcrypt_get_key_size(self::$cipher, self::$mode));
-        $iv = substr($keyHash, 0, mcrypt_get_block_size(self::$cipher, self::$mode));
+        $key     = substr($keyHash, 0, mcrypt_get_key_size(self::$cipher, self::$mode));
+        $iv      = substr($keyHash, 0, mcrypt_get_block_size(self::$cipher, self::$mode));
         return base64_encode(mcrypt_encrypt(self::$cipher, $key, $data, self::$mode, $iv));
     }
 
@@ -57,8 +40,8 @@ class MCryptEncryption implements EncryptionInterface {
      */
     public function decrypt(string $data) {
         $keyHash = md5(Config::getConfig()->encrypt->key);
-        $key = substr($keyHash, 0, mcrypt_get_key_size(self::$cipher, self::$mode));
-        $iv = substr($keyHash, 0, mcrypt_get_block_size(self::$cipher, self::$mode));
+        $key     = substr($keyHash, 0, mcrypt_get_key_size(self::$cipher, self::$mode));
+        $iv      = substr($keyHash, 0, mcrypt_get_block_size(self::$cipher, self::$mode));
         return mcrypt_decrypt(self::$cipher, $key, base64_decode($data), self::$mode, $iv);
     }
 
