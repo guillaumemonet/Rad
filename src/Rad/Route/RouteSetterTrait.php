7ab8ab01dccfe6cb57058d00abf17863fa6e3efb
Guillaume Monet <guillaume.monet@free.fr>
2023-02-11 10:00:48 +0100

    Change Licence Format

--- src/Rad/Route/RouteSetterTrait.php
+++ src/Rad/Route/RouteSetterTrait.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * @license http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
+ * @author Guillaume Monet
+ * @link https://github.com/guillaumemonet/Rad
+ * @package Rad
+ */
+
 namespace Rad\Route;
 
 use Rad\Middleware\Base\AllowHeaders;
