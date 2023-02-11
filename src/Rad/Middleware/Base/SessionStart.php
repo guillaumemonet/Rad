c8258819c0909f8a0a6b86a5f06a7555df612bed
Guillaume Monet <guillaume.monet@free.fr>
2023-02-11 10:03:57 +0100

    Change Licence Format

--- src/Rad/Middleware/Base/SessionStart.php
+++ src/Rad/Middleware/Base/SessionStart.php
@@ -1,33 +1,17 @@
 <?php
 
-/*
- * The MIT License
- *
- * Copyright 2019 guillaume.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+/**
+ * @license http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
+ * @author Guillaume Monet
+ * @link https://github.com/guillaumemonet/Rad
+ * @package Rad
  */
 
 namespace Rad\Middleware\Base;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Rad\Log\Log;
 use Rad\Middleware\MiddlewareBefore;
 use Rad\Route\Route;
 use Rad\Session\Session;
@@ -47,6 +31,7 @@ class SessionStart extends MiddlewareBefore {
      */
     public function middle(ServerRequestInterface $request, ResponseInterface $response, Route $route): ResponseInterface {
         Session::getHandler()->start();
+        Log::getHandler()->debug("Start Session " . session_id());
         return $response;
     }
 
--- src/Rad/Middleware/MiddlewareAfter.php
+++ src/Rad/Middleware/MiddlewareAfter.php
@@ -2,6 +2,9 @@
 
 /**
  * @license http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
+ * @author Guillaume Monet
+ * @link https://github.com/guillaumemonet/Rad
+ * @package Rad
  */
 
 namespace Rad\Middleware;
