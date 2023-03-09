753e36274d04f722d70eebe69e8054e077763932
Guillaume Monet <guillaume.monet@free.fr>
2023-03-09 07:35:56 +0100

    Correct Codec

--- src/Rad/Codec/Codec.php
+++ src/Rad/Codec/Codec.php
@@ -1,32 +1,15 @@
 <?php
 
-/*
- * The MIT License
- *
- * Copyright 2017 guillaume.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+/**
+ * @license http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
+ * @author Guillaume Monet
+ * @link https://github.com/guillaumemonet/Rad
+ * @package Rad
  */
 
 namespace Rad\Codec;
 
-use Rad\Http\Header\AcceptHeader;
+use Rad\Http\HttpHeaders;
 use Rad\Service\Service;
 use Rad\Utils\Mime;
 
@@ -50,7 +33,7 @@ final class Codec extends Service {
     }
 
     public static function matchCodec($acceptHeader) {
-        $mimeArray = AcceptHeader::parse(implode(',', $acceptHeader));
+        $mimeArray      = HttpHeaders::parseAccepted(implode(',', $acceptHeader));
         $availableCodec = array_keys(Codec::getInstance()->services);
         foreach ($mimeArray as $mime => $weight) {
             $shortMime = current(Mime::getMimeTypesFromLong($mime));
@@ -62,6 +45,3 @@ final class Codec extends Service {
     }
 
 }
-
-//Config::load('config/config.json');
-//error_log(print_r(Codec::matchCodec("application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5"), true));
