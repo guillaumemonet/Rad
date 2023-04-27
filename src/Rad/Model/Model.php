f44e84db85bb4164e4ddc2b9c0cbc4788bf8f514
Guillaume Monet <guillaume.monet@free.fr>
2023-04-28 00:26:58 +0200

    Rewrite model base class

--- src/Rad/Model/Model.php
+++ src/Rad/Model/Model.php
@@ -10,8 +10,6 @@
 namespace Rad\Model;
 
 use JsonSerializable;
-use Rad\Config\Config;
-use stdClass;
 
 /**
  * Description of IObject.
@@ -19,85 +17,15 @@ use stdClass;
  * @author Guillaume Monet
  * 
  */
-abstract class Model extends ModelDAO implements JsonSerializable {
-
-    public $resource_uri;
-    public $resource_name;
-    public $resource_namespace;
-
-    public function getID() {
-        return null;
-    }
-
-    public function getResourceName() {
-        return $this->resource_name;
-    }
-
-    public function getResourceURI() {
-        return $this->resource_uri;
-    }
+abstract class Model implements JsonSerializable {
 
     public function jsonSerialize() {
-        $this->generateResourceURI();
         return $this;
     }
 
-    public function generateResourceURI() {
-        $this->resource_uri = Config::getApiConfig()->url . '/' . strtolower($this->getResourceName()) . '/' . $this->getID();
-    }
-
-    /**
-     * 
-     * @param type $object
-     * @return mixed
-     */
-    public final static function hydrate($object) {
-        if (is_object($object)) {
-            $model = self::createObject($object);
-            self::buildObjectContent($object, $model);
-            return $model;
-        } else if (is_array($object)) {
-            $array = [];
-            self::buildArrayContent($object, $array);
-            return $array;
-        } else {
-            return $object;
-        }
-    }
-
-    /**
-     * 
-     * @param type $object
-     * @return mixed
-     */
-    private static function createObject($object) {
-        $model = new stdClass();
-        if (isset($object->resource_name)) {
-            $className = $object->resource_namespace . "\\" . $object->resource_name;
-            $model     = new $className();
-        }
-        return $model;
-    }
-
-    /**
-     * 
-     * @param type $object
-     * @param type $model
-     */
-    private static function buildObjectContent($object, $model) {
-        array_walk($object, function ($val, $key) use ($model) {
-            $model->$key = self::hydrate($val);
-        });
-    }
-
-    /**
-     * 
-     * @param type $object
-     * @param array $array
-     */
-    private static function buildArrayContent($object, array &$array) {
-        array_walk($object, function ($val, $key) use (&$array) {
-            $array[$key] = self::hydrate($val);
+    public function hydrate(array $datas) {
+        array_walk($datas, function ($val, $key) {
+            $this->{$key} = $val;
         });
     }
 
