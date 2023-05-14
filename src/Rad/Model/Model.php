e65362600a854dc34940c2fab20244745ddb6cee
Guillaume Monet <guillaume.monet@free.fr>
2023-05-14 10:41:40 +0200

    Change Model

--- src/Rad/Model/Model.php
+++ src/Rad/Model/Model.php
@@ -10,6 +10,8 @@
 namespace Rad\Model;
 
 use JsonSerializable;
+use Rad\Utils\StringUtils;
+use ReflectionClass;
 
 /**
  * Description of IObject.
@@ -17,9 +19,34 @@ use JsonSerializable;
  * @author Guillaume Monet
  * 
  */
-abstract class Model implements JsonSerializable {
+class Model implements JsonSerializable {
+
+    public $resource_uri;
+    public $resource_name;
+    public $resource_namespace;
+
+    public function __construct() {
+        $this->generateResource();
+    }
+
+    public function getId() {
+        return null;
+    }
+
+    public function getResourceName() {
+        return $this->resource_name;
+    }
+
+    public function getResourceNamespace() {
+        return $this->resource_namespace;
+    }
+
+    public function getResourceURI() {
+        return $this->resource_uri;
+    }
 
     public function jsonSerialize() {
+        $this->generateResource();
         return $this;
     }
 
@@ -27,6 +54,13 @@ abstract class Model implements JsonSerializable {
         array_walk($datas, function ($val, $key) {
             $this->{$key} = $val;
         });
+    }
+
+    public function generateResource() {
+        $reflex                   = new ReflectionClass($this);
+        $this->resource_name      = $reflex->getShortName();
+        $this->resource_namespace = $reflex->getName();
+        $this->resource_uri       = '/' . StringUtils::slugify($this->resource_name) . '/' . $this->getId() ?? '';
     }
 
 }
